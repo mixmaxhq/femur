132e541570088d7a1bf354ecb2e21246c1b4e3d3
Eli Skeggs <eli@mixmax.com>
2020-01-23 14:21:06 -0800

    style: apply .prettierrc with mixmax dev repo prettier-init

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -8,34 +8,36 @@ function isNumber(n) {
 }
 
 describe('femur', () => {
-  let durationAssert = (dur) => {
+  const durationAssert = (dur) => {
     expect(isNumber(dur)).toEqual(true);
   };
 
   it('should be able to time a function call', (done) => {
     // This test will timeout if the done callback isn't called.
-    let wrappedMax = femur.wrap(Math.max, durationAssert);
-    let val = wrappedMax(4, 5);
+    const wrappedMax = femur.wrap(Math.max, durationAssert);
+    const val = wrappedMax(4, 5);
     expect(val).toEqual(5);
     done();
   });
 
   it('should be able to time a function with no arguments', (done) => {
     // This test will timeout if the done callback isn't called.
-    let wrappedFn = femur.wrap(function(){return 5;}, durationAssert);
-    let val = wrappedFn();
+    const wrappedFn = femur.wrap(function() {
+      return 5;
+    }, durationAssert);
+    const val = wrappedFn();
     expect(val).toEqual(5);
     done();
   });
 
   it('should be able to time a function with a callback', (done) => {
     // This test will timeout if the done callback isn't called.
-    let fn = function(cb) {
+    const fn = function(cb) {
       cb(5);
     };
-    let wrappedFn = femur.wrap(fn, durationAssert);
+    const wrappedFn = femur.wrap(fn, durationAssert);
 
-    let cb = function(five) {
+    const cb = function(five) {
       expect(five).toEqual(5);
       done();
     };
